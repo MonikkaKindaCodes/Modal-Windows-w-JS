de45714901e02a94282a92c156840610625baa4c
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-03-17 15:35:46 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,27 @@ const closeModalBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 
+//Defining functions for repetative functions.
+const hiddenModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
 console.log(showModalBtn);
 
 //For loop to access modals
 for (let i = 0; i < showModalBtn.length; i++) {
-    showModalBtn[i].addEventListener('click', function
-        () {
-        console.log('button-clicked');
-        //Unhiding Hidden Classes
-        modal.classList.remove('hidden'); //This removes the hidden class. Do not use the '.' here as you would in the querySelector
-        overlay.classList.remove('hidden');
-    });
+    showModalBtn[i].addEventListener('click', openModal);
 }
+
+closeModalBtn.addEventListener('click', hiddenModal);
+
+overlay.addEventListener('click', hiddenModal);
+
+//Adding a Keypress Event
+
